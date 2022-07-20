bd1f748b88812e5872c79c8ce4e4d2941551ea84
Бородинский Алексей <a-borodinskiy@mail.ru>
2022-07-20 19:05:52 +0300

    Цветовая схема

---
+++ src/ru/netology/graphics/image/TextColorSchemas.java
@@ -0,0 +1,28 @@
+package ru.netology.graphics.image;
+
+public class TextColorSchemas implements TextColorSchema{
+
+    //todo Подумать как улучшить
+
+    @Override
+    public char convert(int color) {
+        if (color >= 0 && color < 32 ) {
+            return '#';
+        } else if (color >= 32 && color < 64) {
+            return '$';
+        } else if (color >= 64 && color < 96) {
+            return '@';
+        } else if (color >= 96 && color < 128) {
+            return '%';
+        } else if (color >= 128 && color < 160) {
+            return '*';
+        } else if (color >= 160 && color < 192) {
+            return '+';
+        } else if (color >= 192 && color < 224) {
+            return '-';
+        } else if (color >= 224 && color < 256) {
+            return '/';
+        } else
+        return 0;
+    }
+}
---
+++ src/ru/netology/graphics/image/TextGraphicsConverters.java
@@ -0,0 +1,30 @@
+package ru.netology.graphics.image;
+
+import java.io.IOException;
+
+public class TextGraphicsConverters implements TextGraphicsConverter {
+    @Override
+    public String convert(String url) throws IOException, BadImageSizeException {
+        return null;
+    }
+
+    @Override
+    public void setMaxWidth(int width) {
+
+    }
+
+    @Override
+    public void setMaxHeight(int height) {
+
+    }
+
+    @Override
+    public void setMaxRatio(double maxRatio) {
+
+    }
+
+    @Override
+    public void setTextColorSchema(TextColorSchema schema) {
+
+    }
+}
