de138eda6923643bf6c7ab0b6504b6ceeed8813d
Бородинский Алексей <a-borodinskiy@mail.ru>
2022-07-21 18:45:03 +0300

    21.07 Вызвал метод

--- src/ru/netology/graphics/Main.java
+++ src/ru/netology/graphics/Main.java
@@ -22,7 +22,7 @@ public class Main {
         schemas.convert(245);
 
 
-      //   Или то же, но с выводом на экран:
+//      //   Или то же, но с выводом на экран:
 //        String url = "https://raw.githubusercontent.com/netology-code/java-diplom/main/pics/simple-test.png";
 //        String imgTxt = converter.convert(url);
 //        System.out.println(imgTxt);
