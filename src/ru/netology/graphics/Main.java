78391de9fcdffb48c40d81ac16d1cc2d1d207d8a
Бородинский Алексей <a-borodinskiy@mail.ru>
2022-07-21 18:19:20 +0300

    21.07 Перенос данных

--- src/ru/netology/graphics/Main.java
+++ src/ru/netology/graphics/Main.java
@@ -1,8 +1,11 @@
 package ru.netology.graphics;
 
+import ru.netology.graphics.image.TextColorSchema;
+import ru.netology.graphics.image.TextColorSchemas;
 import ru.netology.graphics.image.TextGraphicsConverter;
 import ru.netology.graphics.server.GServer;
 
+import javax.swing.*;
 import java.io.File;
 import java.io.PrintWriter;
 
@@ -13,9 +16,14 @@ public class Main {
         GServer server = new GServer(converter); // Создаём объект сервера
         server.start(); // Запускаем
 
-        // Или то же, но с выводом на экран:
-        //String url = "https://raw.githubusercontent.com/netology-code/java-diplom/main/pics/simple-test.png";
-        //String imgTxt = converter.convert(url);
-        //System.out.println(imgTxt);
+        //todo Как вызвать метод???
+//        TextColorSchemas schemas = new TextColorSchemas();
+//        schemas.convert(int color);
+
+
+      //   Или то же, но с выводом на экран:
+//        String url = "https://raw.githubusercontent.com/netology-code/java-diplom/main/pics/simple-test.png";
+//        String imgTxt = converter.convert(url);
+//        System.out.println(imgTxt);
     }
 }
